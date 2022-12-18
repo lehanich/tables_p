fdc847c01e13f6367bef1be0b405d86159851d52
a.yakimkin <ayakimkin@gmail.com>
2022-12-18 21:55:55 +0300

    page update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,143 +1,17 @@
 <script>
-  import { onMount,setContext } from 'svelte';
-    // export let name;
+  import { onMount } from 'svelte';
   import Header from "../components/header/index.svelte";
   import Toolbar from "../components/toolbar/index.svelte";
   import Formula from "../components/formula/index.svelte";
   import Table from "../components/table/index.svelte";
-
-  import {repeat, filter, seq, once, any, on, every, onlyEvent, onlyEvents } from "../lib/eventIter.js";
-
-  let cells
-  let show;
-  setContext("show", {
-      getShow: () => show,
-      getCells: () => cells
-    })
-
-  let table;
+  import Bottom from "../components/bottom/index.svelte";
 
   const loadWorker = async () => {
-    var ball = document.getElementById('my-box');
-
-    (async () => {
-      const dnd = 
-        repeat(() => 
-          filter(
-            seq(
-              once(ball, 'mousedown'),
-              every(
-                any(
-                  on(document.body, 'mousemove'),
-                  on(ball, 'mouseup')
-                ),
-                onlyEvent('mousemove')
-              )
-            ),
-            onlyEvent('mousemove')
-          )
-        );
-   
-      var shiftX = 0;
-      var shiftY = 0;
-
-      for await (const ev of dnd) {
-        console.log(ev)
-        const { top, left } = getCoords(ball);
-        // var shiftX = ev.pageX - top;
-        // var shiftY = ev.pageY - left;
-        
-       ball.style.left = ev.pageX - shiftX + 'px';
-        ball.style.top = ev.pageY - shiftY + 'px';
-
-      }
-
-    })()
-   
-
-    function clear(e) {
-      console.log("!!!", e)
-      e = null
-    }
-
-    ball.ondragstart = function() {
-      return false;
-    };
-
-    function getCoords(elem) {   // кроме IE8-
-      var box = elem.getBoundingClientRect();
-      return {
-        top: box.top + pageYOffset,
-        left: box.left + pageXOffset
-      };
-    }
-
-    // var cell = document.getElementsByClassName('cell');
-    var table = document.getElementsByClassName('table')[0];
-    // console.log(cell);
-    console.log(table);
-
-     show = 
-        repeat(() => 
-          filter(
-            seq(
-              once(table, 'mousedown'),
-              every(
-                any(
-                  on(table, 'mousemove'),
-                  on(table, 'mouseup')
-                ),
-                onlyEvent('mousemove')
-              )
-            ),
-            onlyEvents('mousedown','mousemove')
-          )
-        );
-    
-    // (async () => {
-    //   const show = 
-    //     repeat(() => 
-    //       filter(
-    //         seq(
-    //           once(table, 'mousedown'),
-    //           every(
-    //             any(
-    //               on(table, 'mousemove'),
-    //               on(table, 'mouseup')
-    //             ),
-    //             onlyEvent('mousemove')
-    //           )
-    //         ),
-    //         onlyEvent('mousemove')
-    //       )
-    //     );
-   
-    //   var shiftX = 0;
-    //   var shiftY = 0;
-
-    //   for await (const ev of show) {
-    //     console.log(ev.currentTarget)
-    //     // ev.currentTarget.
-    //     // const { top, left } = getCoords(ball);
-    //     // var shiftX = ev.pageX - top;
-    //     // var shiftY = ev.pageY - left;
-        
-    //   //  ball.style.left = ev.pageX - shiftX + 'px';
-    //   //   ball.style.top = ev.pageY - shiftY + 'px';
-
-    //   }
-
-    // })()
-    console.log(5,cells) //table
-  };
-
-
-  // const loadWorker = async () => {
   //   const SyncWorker = await import('$lib/my.js?worker');
   //   syncWorker = new SyncWorker.default();
 
   //   syncWorker.postMessage({});
-  // };
+  };
 
   onMount(loadWorker);
 </script>
@@ -147,29 +21,10 @@
   Header
   Toolbar
   Formula
-  Table(bind:this="{table}" bind:cells="{cells}")
-  div#my-box BALL
+  Table
+  Bottom
 </template>
 
 <style lang="scss" module>
-  // @import "../layout/styles/index.scss";
 
-  #my-box {
-    position: absolute;
-    z-index: 100;
-
-  }
-  // $color: red;
-
-  // h1 {
-  //   color: $color;
-  // }
-
-  // div {
-  //   background: green;
-
-  //   // > p {
-  //   //   color: #fff;
-  //   // }
-  // }
 </style>
