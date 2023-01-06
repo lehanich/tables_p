569168e81823571eed61fd88abddd367a3173a7c
a.yakimkin <ayakimkin@gmail.com>
2023-01-06 16:19:51 +0300

    update store (matrix, coordinates, data styles)

--- src/lib/data/storeTableMatrix.ts
+++ src/lib/data/storeTableMatrix.ts
@@ -2,8 +2,8 @@ import { writable } from 'svelte/store';
 import { browser } from "$app/environment";
 import Matrix from "./base/Matrix";
 
-export const tableStore = (key, initial: number) => {                 // receives the key of the local storage and an initial value
-  const tableData = new Matrix(26,21);
+export const tableStoreMatrix = (key, initial: any[]) => {                 // receives the key of the local storage and an initial value
+  let tableData = new Matrix(initial[0],initial[1],initial[2])//(...initial);
 
   // for (let i=0; i < initial; i++) {
   //   tableData.push([])
@@ -31,7 +31,28 @@ export const tableStore = (key, initial: number) => {                 // receive
       //   console.log(1, value)
       //   return value;
       // }
-      update
+      update,
+      areaModify(area, delta) {
+        return tableData.areaModify(area, delta);
+      },
+      getMatrix(area) {
+        return tableData.getMatrix(area);
+      },
+      getElement(x,y) {
+        return tableData.getElement(x,y);
+      },
+      set setElement(val) {
+        const {x, y, value} = val
+        tableData.setElement(x, y, value);
+        tableData=tableData;
+      },
+      updateMatrix(area, buffer) {
+        tableData.updateMatrix(area, buffer);
+        tableData=tableData;
+      },
+      print() {
+        return tableData.print();
+      }
   }
   
 }
