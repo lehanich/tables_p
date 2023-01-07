0cda3fe8c30a4c3eeb443f61cf8e755fe83c1393
a.yakimkin <ayakimkin@gmail.com>
2023-01-07 11:59:26 +0300

    add store

--- src/components/formula/index.svelte
+++ src/components/formula/index.svelte
@@ -1,7 +1,28 @@
+<script lang="ts">
+  import { onMount, setContext, afterUpdate, tick } from 'svelte';
+  import { stateTableMatrix, stateCoordinates, inputStore } from "../../lib/data/stores";
+
+  afterUpdate(() => {
+    console.log($stateCoordinates)
+    // $stateTable[$stateCoordinates.select[0][1]-1][$stateCoordinates.select[0][0]] = $inputStore
+    $stateTableMatrix.update( $stateCoordinates.select[0][0], $stateCoordinates.select[0][1]-1).element = $inputStore
+    $stateTableMatrix = $stateTableMatrix
+    console.log($inputStore)
+    // console.log($stateTable[$stateCoordinates.select[0][1]-1][$stateCoordinates.select[0][0]])
+  })
+
+  // $stateTable[$stateCoordinates.select[0][0]][$stateCoordinates.select[0][1]] = $inputStore
+
+</script>
+
 <template lang="pug">
   div.formula
     div.info
-    div.input(contenteditable spellcheck="false")
+    div.input(
+      contenteditable
+      spellcheck="false"
+      bind:innerHTML="{$inputStore}"
+    )
 </template>
 
 <style lang="scss" module>
