7e8ad8736b125fd3ff0e5f8c19f34a0b4d355406
a.yakimkin <ayakimkin@gmail.com>
2023-01-08 11:03:47 +0300

    helper caret position

---
+++ src/lib/helpers.ts
@@ -0,0 +1,16 @@
+
+export function setCaretPosition(ctrl, pos) {
+  // Modern browsers
+  if (ctrl.setSelectionRange) {
+    ctrl.focus();
+    ctrl.setSelectionRange(pos, pos);
+  
+  // IE8 and below
+  } else if (ctrl.createTextRange) {
+    const range = ctrl.createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', pos);
+    range.moveStart('character', pos);
+    range.select();
+  }
+}
