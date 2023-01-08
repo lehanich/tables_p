005ff159d1b3df1fe7858d180f97fdda36aca0fa
a.yakimkin <ayakimkin@gmail.com>
2023-01-08 11:05:22 +0300

    storeTable - for dispaly(draw) table

--- src/lib/data/storeTable.ts
+++ src/lib/data/storeTable.ts
@@ -1,15 +1,25 @@
 import { writable } from 'svelte/store';
 import { browser } from "$app/environment";
+import { toChar } from "../tableHeplers";
 
 export const tableStore = (key, initial: number) => {                 // receives the key of the local storage and an initial value
-  const tableData = [];
-
-  for (let i=0; i < initial; i++) {
-    tableData.push([])
+  const CODES = {
+    A: 65,
+    Z: 90,
   }
+  const tableStructure = {
+    CODES,
+    colsCount: CODES.Z - CODES.A + 1,
+    cols: [],
+    rows: []
+  };
 
-  const { subscribe, set, update } = writable(tableData);
+  const { subscribe, set, update } = writable(tableStructure);
   
+  for(let i=0; i< tableStructure.colsCount; i++) { tableStructure.cols.push(toChar(tableStructure.cols,i))}
+  for(let i=0; i< initial; i++) { tableStructure.rows.push("")}
+
+
   return {
       subscribe,
       set: (value) => {
