427d2cbdc12a22511eb613b7f4f5afe6019732db
a.yakimkin <ayakimkin@gmail.com>
2023-01-13 14:12:58 +0300

    update for cel union

--- src/components/table/cell.svelte
+++ src/components/table/cell.svelte
@@ -1,11 +1,18 @@
 <!-- <svelte:options accessors immutable/> -->
 <script lang="ts">
-  import { afterUpdate } from 'svelte';
-
   export let cell: HTMLDivElement;
   export let html: string = "";
 
   const { value, row, column } = $$props
+
+  export let width;
+  export let display;
+  export let height;
+
+  $: showDisplay = display === "block" ? 1 : 0;
+  $: showWidth = width ? width : "120px";
+  $: showHeight = height ? height : "100%";
+
 </script>
 
 <template lang="pug">
@@ -13,6 +20,10 @@
     data-row="{row}"
     data-column="{column}"
     bind:this='{cell}'
+    style:z-index='{showDisplay}'
+    style:width='{showWidth}'
+    style:height='{showHeight}'
+    style:background-color="#fff"
   )
     slot
 </template>
